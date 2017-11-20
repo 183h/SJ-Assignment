5673d8eac77b8a6b8cd6e6e7948f06b41a9b6cc2
mpuk <puk.matej@gmail.com>
2017-11-20 15:05:57 +0100

    transition table

---
+++ transition_table.py
@@ -0,0 +1,125 @@
+table = {
+    "url": {
+        "http://": "httpaddress", "ftp://": "ftpaddress", "telnet://": "telnetaddress", "mailto::": "mailtoaddress"
+    },
+    "httpaddress": {
+        "http://": "http:// hostport httpaddress1"
+    },
+    "httpaddress1": {
+        "http://": "/ path httpaddress2", "?": "? search", "$": "epsilon"
+    },
+    "httpaddress2": {
+        "?": "? search", "$": "epsilon"
+    },
+    "ftpaddress": {
+        "ftp://": "ftp:// login / path"
+    },
+    "telnetaddress": {
+        "telnet://": "telnet:// login"
+    },
+    "mailtoaddress": {
+        "mailto::": "mailto:: xalphas @ hostname"
+    },
+    "login": {
+
+    },
+    "login1": {
+
+    },
+    "login2": {
+
+    },
+    "hostport": {
+
+    },
+    "hostport1": {
+
+    },
+    "hostname": {
+
+    },
+    "hostname1": {
+
+    },
+    "port": {
+
+    },
+    "path": {
+
+    },
+    "path1": {
+
+    },
+    "search": {
+
+    },
+    "search4": {
+
+    },
+    "password": {
+
+    },
+    "segment": {
+
+    },
+    "xalphas": {
+
+    },
+    "xalphas1": {
+
+    },
+    "xalpha": {
+
+    },
+    "digits": {
+
+    },
+    "digits1": {
+
+    },
+    "alpha": {
+
+    },
+    "digit": {
+
+    },
+    "http://": {
+
+    },
+    "?": {
+
+    },
+    "ftp://": {
+
+    },
+    "telnet://": {
+
+    },
+    "mailto::": {
+
+    },
+    "@": {
+
+    },
+    ":": {
+
+    },
+    ".": {
+
+    },
+    "/": {
+
+    },
+    "+": {
+
+    },
+    "A-Za-z": {
+
+    },
+    "0..9": {
+
+    },
+    "epsilon": {
+
+    },
+}
