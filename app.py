4552606377601b9be55d33c3523c289175a6258c
mpuk <puk.matej@gmail.com>
2017-11-27 09:42:01 +0100

    added app.py for gui

---
+++ app.py
@@ -0,0 +1,16 @@
+from Tkinter import *
+
+
+class Application(Frame):
+    def createWidgets(self):
+        pass
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
