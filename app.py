faecc2b284f94e9baf672b924e718bcebf54b709
mpuk <puk.matej@gmail.com>
2017-11-27 15:43:43 +0100

    api for parsing

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
+from transition_table import table
+from simple_url_parser import SimpleUrlParser
+from simple_url_lexer import SimpleUrlLexer
 
 app = Flask(__name__)
+lexer = SimpleUrlLexer()
+lexer.build()
 
 
 @app.route("/")
@@ -12,7 +17,13 @@ def main():
 def parse():
     q = request.form.get('q', default="", type=str)
 
+    lexer.lexical_analysis1(q)
+
+    parser = SimpleUrlParser(lexer.output)
+    parser.parse()
+
     return render_template(
         'parse.html',
-        q=q
+        q=q,
+        data=parser.output
     )
